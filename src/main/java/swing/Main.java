21be8e6c8502def2fcf6424f6806e650a7d5f748
Gáspár Róbert <gasparrobert0908@gmail.com>
2024-10-14 20:23:42 +0200

    added Main ad switched to java 21

---
+++ src/main/java/swing/Main.java
@@ -0,0 +1,13 @@
+package swing;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
