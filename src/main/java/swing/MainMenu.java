aba93407e1d6289414195a82dd1cb85a9a9e2046
Gáspár Róbert <gasparrobert0908@gmail.com>
2024-10-15 21:27:00 +0200

    created MainMenu

--- src/main/java/swing/MainMenu.java
+++ src/main/java/swing/MainMenu.java
@@ -1,9 +1,13 @@
 package swing;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class MainMenu extends JPanel {
     private final CardLayoutSwitcher switcher;
@@ -15,16 +19,34 @@ public class MainMenu extends JPanel {
     }
 
     private void render() {
-        JButton button = new JButton("Click Me");
 
-        button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                switcher.registerActivePanel("esc");
-                switcher.switchTo("grid");
-            }
-        });
 
-        this.add(button);
+        this.setLayout(new BorderLayout());
+
+        JPanel menuPanel = new JPanel(new BorderLayout());
+        menuPanel.setLayout(new BorderLayout(10, 10));
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new GridLayout(3, 1, 10, 10));
+
+        JButton playButton = new JButton("Play");
+        JButton loadGameButton = new JButton("Load Game");
+        JButton exitButton = new JButton("Exit");
+
+        buttonPanel.add(playButton);
+        buttonPanel.add(loadGameButton);
+        buttonPanel.add(exitButton);
+
+        buttonPanel.setBorder(BorderFactory.createEmptyBorder(0, 30, 30, 30));
+
+        // Create the title label
+        JLabel titleLabel = new JLabel("The Game Of Life", JLabel.CENTER);
+        titleLabel.setFont(new Font("Times New Roman", Font.PLAIN, 36));
+
+        // Add the title to the top and buttons to the center
+        menuPanel.add(titleLabel, BorderLayout.NORTH);
+        menuPanel.add(buttonPanel, BorderLayout.CENTER);
+
+        this.add(menuPanel);
     }
 }
