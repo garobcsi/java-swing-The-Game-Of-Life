960e70469c72856d876e329aacd7b7fb80911092
Gáspár Róbert <gasparrobert0908@gmail.com>
2024-10-15 21:45:31 +0200

    created placeholder PauseMenu

--- src/main/java/swing/PauseMenu.java
+++ src/main/java/swing/PauseMenu.java
@@ -18,10 +18,31 @@ public class PauseMenu extends JPanel implements KeyListener {
     }
 
     private void render() {
-        JLabel label = new JLabel("Pause Menu");
-        label.setFont(new Font("Times New Roman", Font.PLAIN, 30));
+        this.setLayout(new BorderLayout());
 
-        this.add(label);
+        JPanel menuPanel = new JPanel(new BorderLayout());
+        menuPanel.setLayout(new BorderLayout(10, 10));
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new GridLayout(3, 1, 10, 10));
+
+        JButton backButton = new JButton("Back");
+        JButton saveGameButton = new JButton("Save Game");
+        JButton mainMenuButton = new JButton("Back To The Main Menu");
+
+        buttonPanel.add(backButton);
+        buttonPanel.add(saveGameButton);
+        buttonPanel.add(mainMenuButton);
+
+        buttonPanel.setBorder(BorderFactory.createEmptyBorder(0, 30, 30, 30));
+
+        JLabel titleLabel = new JLabel("Pause Menu", JLabel.CENTER);
+        titleLabel.setFont(new Font("Times New Roman", Font.PLAIN, 30));
+
+        menuPanel.add(titleLabel, BorderLayout.NORTH);
+        menuPanel.add(buttonPanel, BorderLayout.CENTER);
+
+        this.add(menuPanel);
     }
 
     @Override
