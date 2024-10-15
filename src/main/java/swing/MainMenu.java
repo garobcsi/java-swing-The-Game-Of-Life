5de36d0962daf09f9da1d8504f9361953e94f28e
Gáspár Róbert <gasparrobert0908@gmail.com>
2024-10-15 21:44:55 +0200

    created basic placeholder button actions for MainMenu

--- src/main/java/swing/MainMenu.java
+++ src/main/java/swing/MainMenu.java
@@ -1,13 +1,10 @@
 package swing;
 
-import java.awt.BorderLayout;
-import java.awt.Font;
-import java.awt.GridLayout;
-
-import javax.swing.BorderFactory;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import javax.swing.*;
 
 public class MainMenu extends JPanel {
     private final CardLayoutSwitcher switcher;
@@ -19,8 +16,6 @@ public class MainMenu extends JPanel {
     }
 
     private void render() {
-
-
         this.setLayout(new BorderLayout());
 
         JPanel menuPanel = new JPanel(new BorderLayout());
@@ -39,14 +34,29 @@ public class MainMenu extends JPanel {
 
         buttonPanel.setBorder(BorderFactory.createEmptyBorder(0, 30, 30, 30));
 
-        // Create the title label
         JLabel titleLabel = new JLabel("The Game Of Life", JLabel.CENTER);
         titleLabel.setFont(new Font("Times New Roman", Font.PLAIN, 36));
 
-        // Add the title to the top and buttons to the center
         menuPanel.add(titleLabel, BorderLayout.NORTH);
         menuPanel.add(buttonPanel, BorderLayout.CENTER);
 
         this.add(menuPanel);
+
+        playButton.addActionListener(event -> {
+            switcher.switchTo("matrixSize");
+
+        });
+
+        loadGameButton.addActionListener(event -> {
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setFileFilter(new javax.swing.filechooser.FileNameExtensionFilter("JSON File", "json"));
+            int result = fileChooser.showOpenDialog(null);
+
+            if (result == JFileChooser.APPROVE_OPTION) {
+                File file = fileChooser.getSelectedFile();
+            }
+        });
+
+        exitButton.addActionListener(event -> System.exit(0));
     }
 }
