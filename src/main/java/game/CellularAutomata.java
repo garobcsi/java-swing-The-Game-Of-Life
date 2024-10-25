263091732700138acf65ba47293ec588d9679a8d
Gáspár Róbert <gasparrobert0908@gmail.com>
2024-10-25 17:39:26 +0200

    Made celluar automata

---
+++ src/main/java/game/CellularAutomata.java
@@ -0,0 +1,79 @@
+package game;
+
+import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class CellularAutomata extends JPanel implements KeyListener {
+    private final boolean[][] matrix;
+
+    int sizeX = 0, sizeY = 0;
+
+    public CellularAutomata(boolean[][] matrix) throws NotMatrixException {
+        this.matrix = matrix;
+        addKeyListener(this);
+        setFocusable(true);
+
+        sizeX = matrix.length;
+        sizeY = matrix[0].length;
+        for (boolean[] i : matrix) {
+            if (i.length != sizeY) throw new NotMatrixException("2D array is not a matrix !");
+        }
+    }
+
+    private int countNeighbors(int x, int y) {
+        int count = 0;
+
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                if (i == 0 && j == 0) continue;
+
+                int nx = (x + i + sizeX) % sizeX;
+                int ny = (y + j + sizeY) % sizeY;
+
+                if (matrix[nx][ny]) count++;
+                if (count > 3) return count;
+            }
+        }
+        return count;
+    }
+
+    public void next() {
+        int sizeX= matrix.length, sizeY = matrix[0].length;
+
+        boolean[][] nextMatrix = new boolean[sizeX][sizeY];
+
+        for (int i = 0; i < sizeX; i++) {
+            for (int j = 0; j < sizeY; j++) {
+                int neighbors = countNeighbors(i, j);
+
+                if (matrix[i][j]) {
+                    nextMatrix[i][j] = (neighbors == 2 || neighbors == 3);
+                } else {
+                    nextMatrix[i][j] = (neighbors == 3);
+                }
+            }
+        }
+
+        for (int i = 0; i < sizeX; i++) {
+            System.arraycopy(nextMatrix[i], 0, matrix[i], 0, sizeY);
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_N) {
+            next();
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+}
---
+++ src/main/java/game/NotMatrixException.java
@@ -0,0 +1,7 @@
+package game;
+
+public class NotMatrixException extends Exception {
+    public NotMatrixException(String message) {
+        super(message);
+    }
+}
