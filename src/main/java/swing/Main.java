6b49b2207a9aa21f14258e0731aef6be946d832a
Gáspár Róbert <gasparrobert0908@gmail.com>
2024-10-25 17:40:30 +0200

    Added CellularAutomata to main

--- src/main/java/swing/Main.java
+++ src/main/java/swing/Main.java
@@ -1,22 +1,26 @@
 package swing;
 
+import game.CellularAutomata;
+import game.NotMatrixException;
+
 import javax.swing.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NotMatrixException {
         JFrame frame = new JFrame("The Game Of Life");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(600, 600);
 
         frame.setLocationRelativeTo(null);
 
-        boolean[][] matrix = new boolean[1][1];
+        boolean[][] matrix = new boolean[40][40];
 
         CardLayoutSwitcher switcher = new CardLayoutSwitcher();
         switcher.addPanel("home", new MainMenu(switcher));
         switcher.addPanel("pause", new PauseMenu(switcher));
         switcher.addPanel("matrixSize", new MatrixSizeMenu(switcher, matrix));
         switcher.addPanel("grid", new ScalableGrid(matrix));
+        switcher.addPanel("nextStep",new CellularAutomata(matrix));
 
         switcher.switchTo("home");
 
