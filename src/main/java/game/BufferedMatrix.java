c98b78a984308700cfd34c83b4c46b561f768c36
Gáspár Róbert <gasparrobert0908@gmail.com>
2024-10-26 16:55:41 +0200

    added serialization to matrix

--- src/main/java/game/BufferedMatrix.java
+++ src/main/java/game/BufferedMatrix.java
@@ -1,13 +1,20 @@
 package game;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Arrays;
 
 public class BufferedMatrix<T> {
     private int x;
     private int y;
     private T[][] currentMatrix;
-    private T[][] nextMatrix;
-    private final T defaultValue;
+    private transient T[][] nextMatrix;
+    private T defaultValue;
 
     public BufferedMatrix(int x, int y, T defaultValue) {
         this.x = x;
@@ -73,5 +80,38 @@ public class BufferedMatrix<T> {
 
     public int getSizeY() {
         return y;
+    }
+
+    public void toJson(File file) {
+        GsonBuilder builder = new GsonBuilder();
+        builder.setPrettyPrinting();
+
+        Gson gson = builder.create();
+        try (FileWriter writer = new FileWriter(file)) {
+            gson.toJson(this,writer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void fromJson(File file) {
+        Gson gson = new GsonBuilder().create();
+        try (FileReader reader = new FileReader(file)) {
+            BufferedMatrix<T> matrix = gson.fromJson(reader,BufferedMatrix.class);
+            this.x = matrix.x;
+            this.y = matrix.y;
+            this.defaultValue = matrix.defaultValue;
+            this.currentMatrix = (T[][]) new Object[x][y];
+            this.nextMatrix = (T[][]) new Object[x][y];
+            fillMatrix(nextMatrix, defaultValue);
+
+            for (int i = 0; i< matrix.x; i++) {
+                for (int j = 0; j < matrix.y; j++) {
+                    this.currentMatrix[i][j] = matrix.currentMatrix[i][j];
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
